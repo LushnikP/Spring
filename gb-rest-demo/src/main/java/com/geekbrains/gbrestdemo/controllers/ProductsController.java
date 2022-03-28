d6d09d4a5ca673db723294b9aa360c29dad6b9a3
Pavel <pasha134134@mail,ru>
2022-03-28 16:47:45 +0600

    hw11

--- gb-rest-demo/src/main/java/com/geekbrains/gbrestdemo/controllers/ProductsController.java
+++ gb-rest-demo/src/main/java/com/geekbrains/gbrestdemo/controllers/ProductsController.java
@@ -2,26 +2,36 @@ package com.geekbrains.gbrestdemo.controllers;
 
 
 import com.geekbrains.gbrestdemo.entities.Product;
+import com.geekbrains.gbrestdemo.entities.User;
 import com.geekbrains.gbrestdemo.services.ProductsService;
+import com.geekbrains.gbrestdemo.services.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Controller
 @RequestMapping("/products")
 public class ProductsController {
     private ProductsService productsService;
+    private UserServiceImpl userService;
+
+    private HashMap<String, Integer> cartProduct = new HashMap<>();
 
     @Autowired
     public void setProductsService(ProductsService productsService) {
         this.productsService = productsService;
+    }
+
+
+    @Autowired
+    public void setUserService(UserServiceImpl userService) {
+        this.userService = userService;
     }
 
     @RequestMapping("/{id}")
@@ -38,6 +48,38 @@ public class ProductsController {
         List<Product> allProducts = productsService.getAllProducts();
         model.addAttribute("productsList", allProducts);
         return "products-list";
+    }
+
+    @RequestMapping("/cart")
+    public String showCart(Model model) {
+        int total = 0;
+        for(Map.Entry<String, Integer> entry : cartProduct.entrySet()){
+            total += entry.getValue();
+        }
+        model.addAttribute("cartList", cartProduct);
+        model.addAttribute("total", total);
+        return "cart";
+    }
+
+    @RequestMapping(value = {"/add_to_cart/{id}"}, method = RequestMethod.GET)
+    public String addToCart(@PathVariable(value = "id") Long productId, Model model) {
+        Product product = productsService.getProductById(productId);
+
+        int count = 1;
+        if(cartProduct.containsKey(product.getTitle())){
+            count++;
+            Integer value = product.getPrice() * count;
+            cartProduct.put(product.getTitle(), value);
+        } else {
+            cartProduct.put(product.getTitle(), product.getPrice());
+        }
+        return "redirect:/products/list";
+    }
+
+    @RequestMapping(path="/remove_from_cart/{key}", method=RequestMethod.GET)
+    public String removeFromCart(@PathVariable(value = "key") String productName) {
+        cartProduct.remove(productName);
+        return "redirect:/products/cart";
     }
 
     @RequestMapping(path="/add", method= RequestMethod.GET)
